4b5033aa026d822f4a36f07fa1bd6b978f18190f
iankaz <iankazembe41@gmail.com>
2025-03-18 23:34:42 +0200

    ...

--- server.js
+++ server.js
@@ -5,43 +5,71 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const expressLayouts = require("express-ejs-layouts") // Added
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const expressLayouts = require("express-ejs-layouts"); // Added
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/index"); // Added to ensure utilities are in scope
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
+
+/* ***********************
+ * Middleware to Fetch Navigation
+ *************************/
+app.use(async (req, res, next) => {
+  res.locals.nav = (await utilities.getNav()) || []; // Make 'nav' accessible globally
+  next(); // Continue to the next middleware
+});
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-app.use(express.static('public'))
+app.use(static);
+app.use(express.static("public"));
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", baseController.buildHome);
 
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  const nav = res.locals.nav; // Use globally accessible navigation
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
--- utilities/index.js
+++ utilities/index.js
@@ -1,80 +1,56 @@
-const invModel = require("../models/inventory-model")
-const Util = {}
+const invModel = require("../models/inventory-model");
+const Util = {};
 
 /* ************************
  * Constructs the nav HTML unordered list
- ************************** */
-Util.getNav = async function (req, res, next) {
-  let data = await invModel.getClassifications()
-  let list = "<ul>"
-  list += '<li><a href="/" title="Home page">Home</a></li>'
+ ************************ */
+Util.getNav = async function () {
+  // Fetch classification data from the inventory model
+  let data = await invModel.getClassifications();
+  let list = "<ul>"; // Begin the unordered list
+
+  // Add a Home link
+  list += '<li><a href="/" title="Home page">Home</a></li>';
+
+  // Dynamically generate links for each classification
   data.rows.forEach((row) => {
-    list += "<li>"
-    list +=
-      '<a href="/inv/type/' +
-      row.classification_id +
-      '" title="See our inventory of ' +
-      row.classification_name +
-      ' vehicles">' +
-      row.classification_name +
-      "</a>"
-    list += "</li>"
-  })
-  list += "</ul>"
-  return list
-}
+    list += "<li>";
+    list += `<a href="/inv/type/${row.classification_id}" title="See our inventory of ${row.classification_name} vehicles">${row.classification_name}</a>`;
+    list += "</li>";
+  });
+
+  list += "</ul>"; // Close the unordered list
+  return list; // Return the constructed HTML
+};
 
 /* **************************************
  * Build the classification view HTML
- * ************************************ */
+ ************************************** */
 Util.buildClassificationGrid = async function (data) {
-  let grid
-  if (data.length > 0) {
-    grid = '<ul id="inv-display">'
-    data.forEach((vehicle) => {
-      grid += '<li>'
-      grid +=
-        '<a href="../../inv/detail/' +
-        vehicle.inv_id +
-        '" title="View ' +
-        vehicle.inv_make +
-        ' ' +
-        vehicle.inv_model +
-        ' details"><img src="' +
-        vehicle.inv_thumbnail +
-        '" alt="Image of ' +
-        vehicle.inv_make +
-        ' ' +
-        vehicle.inv_model +
-        ' on CSE Motors" /></a>'
-      grid += '<div class="namePrice">'
-      grid += "<hr />"
-      grid += "<h2>"
-      grid +=
-        '<a href="../../inv/detail/' +
-        vehicle.inv_id +
-        '" title="View ' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        ' details">' +
-        vehicle.inv_make +
-        " " +
-        vehicle.inv_model +
-        "</a>"
-      grid += "</h2>"
-      grid +=
-        "<span>$" +
-        new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
-        "</span>"
-      grid += "</div>"
-      grid += "</li>"
-    })
-    grid += "</ul>"
-  } else {
-    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
-  }
-  return grid
-}
+  let grid;
 
-module.exports = Util
+  // Check if data contains vehicles
+  if (data.length > 0) {
+    grid = '<ul id="inv-display">'; // Begin the unordered grid list
+    data.forEach((vehicle) => {
+      grid += "<li>";
+      grid += `<a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details"><img src="${vehicle.inv_thumbnail}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model} on CSE Motors" /></a>`;
+      grid += '<div class="namePrice">';
+      grid += "<hr />";
+      grid += "<h2>";
+      grid += `<a href="../../inv/detail/${vehicle.inv_id}" title="View ${vehicle.inv_make} ${vehicle.inv_model} details">${vehicle.inv_make} ${vehicle.inv_model}</a>`;
+      grid += "</h2>";
+      grid += `<span>$${new Intl.NumberFormat("en-US").format(vehicle.inv_price)}</span>`;
+      grid += "</div>";
+      grid += "</li>";
+    });
+    grid += "</ul>"; // Close the unordered list
+  } else {
+    // Show a message if no vehicles match
+    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
+  }
+
+  return grid; // Return the constructed grid HTML
+};
+
+module.exports = Util;
