63b2bba902e64074f5ae24c7dd5315d289dc3acc
iankaz <iankazembe41@gmail.com>
2025-03-24 18:40:34 +0200

    coming together

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,14 +1,27 @@
 const utilities = require("../utilities")
 
 /* ****************************************
-*  Deliver login view
-* *************************************** */
+ *  Deliver login view
+ * *************************************** */
 async function buildLogin(req, res, next) {
   let nav = await utilities.getNav()
   res.render("account/login", {
     title: "Login",
     nav,
+    errors: null,
   })
 }
 
-module.exports = { buildLogin }
+/* ****************************************
+ *  Deliver registration view
+ * *************************************** */
+async function buildRegister(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null
+  })
+}
+
+module.exports = { buildLogin, buildRegister }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,4 +6,7 @@ const accountController = require("../controllers/accountController")
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
 module.exports = router
